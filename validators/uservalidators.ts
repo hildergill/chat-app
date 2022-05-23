dce7afa8848cd7e4a80ead930f6116ec1334c51c
hildergill <hildergill@gmail.com>
2022-05-23 14:25:13 +0800

    Added the errors localizations

--- validators/uservalidators.ts
+++ validators/uservalidators.ts
@@ -5,12 +5,27 @@ const DisplayNameMaxLength: number = 256;
 const PasswordMinLength: number = 8;
 
 export const SignUpValidator = Joi.object<SignUpBody, true, SignUpBody>({
-	displayName: Joi.string().required().max(DisplayNameMaxLength),
-	password: Joi.string().required().min(PasswordMinLength),
-	confirmPassword: Joi.string().required().min(PasswordMinLength).valid(Joi.ref("password"))
+	displayName: Joi.string().required().max(DisplayNameMaxLength).messages({
+		"string.max": "errrors:inputs.displayName.max"
+	}),
+	password: Joi.string().required().min(PasswordMinLength).messages({
+		"string.min": "errors:inputs.password.min"
+	}),
+	confirmPassword: Joi.string().required().min(PasswordMinLength).valid(Joi.ref("password")).messages({
+		"string.min": "errors:inputs.password.min",
+		"object.valid": "errors:confirmPassword.mismatch"
+	})
+}).messages({
+	"any.required": "errors:inputs.required"
 });
 
 export const LogInValidator = Joi.object<LogInBody, true, LogInBody>({
-	displayName: Joi.string().required().max(DisplayNameMaxLength),
-	password: Joi.string().required().min(PasswordMinLength)
+	displayName: Joi.string().required().max(DisplayNameMaxLength).messages({
+		"string.max": "errrors:inputs.displayName.max"
+	}),
+	password: Joi.string().required().min(PasswordMinLength).messages({
+		"string.min": "errors:inputs.password.min"
+	})
+}).messages({
+	"any.required": "errors:inputs.required"
 });
