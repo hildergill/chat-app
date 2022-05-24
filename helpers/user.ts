8f8feeb716414f924913d63d40e76c8a4921ec5a
hildergill <hildergill@gmail.com>
2022-05-24 20:18:08 +0800

    Replaced the parameterized database connection field to the singleton one

--- helpers/user.ts
+++ helpers/user.ts
@@ -1,31 +1,31 @@
 import { hash } from "bcrypt";
-import { Connection } from "mysql";
 import { v4 } from "uuid";
 import User from "../models/user";
+import DatabaseConnectionSingleton from "../backend/singletons/databaseconnection";
 
 export type CreateUserParams = {
 	displayName: string;
 	password: string;
 };
 
-export const createUser = (databaseConnection: Connection, params: CreateUserParams): Promise<User> =>
+export const createUser = (params: CreateUserParams): Promise<User> =>
 	new Promise<User>(async (resolve, reject) => {
 		const id: string = v4(),
 			password: string = await hash(params.password, 12),
 			{ displayName } = params,
 			queryString: string = "insert users values (?, ?, ?)";
 
-		databaseConnection.query(queryString, [id, displayName, password], (error) => {
+		DatabaseConnectionSingleton.DatabaseConnection.query(queryString, [id, displayName, password], (error) => {
 			if (error) return reject(error);
 			return resolve({ id, displayName, password });
 		});
 	});
 
-export const fetchUserByDisplayName = (databaseConnection: Connection, displayName: String): Promise<User> =>
+export const fetchUserByDisplayName = (displayName: String): Promise<User> =>
 	new Promise<User>((resolve, reject) => {
 		const queryString: string = "select * from users where display_name = ?";
 
-		databaseConnection.query(queryString, displayName, (error, results: any[]) => {
+		DatabaseConnectionSingleton.DatabaseConnection.query(queryString, displayName, (error, results: any[]) => {
 			if (error) return reject(error);
 
 			if (results.length !== 1) return reject(0);
--- helpers/usertoken.ts
+++ helpers/usertoken.ts
@@ -1,13 +1,13 @@
-import { Connection } from "mysql";
+import DatabaseConnectionSingleton from "../backend/singletons/databaseconnection";
 import UserToken from "../models/usertoken";
 import { createHash } from "crypto";
 
-export const createUserToken = (databaseConnection: Connection, user: string): Promise<UserToken> =>
+export const createUserToken = (user: string): Promise<UserToken> =>
 	new Promise<UserToken>(async (resolve, reject) => {
 		const token: string = createHash("SHA256").update(Date.now().toString()).digest("hex"),
 			queryString: string = "insert into user_tokens values (?, ?)";
 
-		databaseConnection.query(queryString, [token, user], (error) => {
+		DatabaseConnectionSingleton.DatabaseConnection.query(queryString, [token, user], (error) => {
 			if (error) return reject(error);
 			return resolve({ token, user });
 		});
