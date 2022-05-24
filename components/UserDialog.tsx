039e5128be567ce5743a204a155e3f0ce209d0b9
hildergill <hildergill@gmail.com>
2022-05-24 10:10:17 +0800

    Some backup commit

--- components/UserDialog.tsx
+++ components/UserDialog.tsx
@@ -1,17 +1,33 @@
+import { useTranslation } from "next-i18next";
 import { PropsWithChildren } from "react";
+import Error from "../models/error";
 
 export type UserDialogProps = PropsWithChildren<{
 	title: string;
+	errors?: Error[];
 }>;
 
 export const UserDialog = (props: UserDialogProps) => {
-	const { title, children }: UserDialogProps = props;
+	const { title, children, errors }: UserDialogProps = props;
+
+	const { t } = useTranslation();
+
+	const getErrorBoxes = (): JSX.Element[] =>
+		errors.map((error: Error, key: number) => {
+			return (
+				<div key={key}>
+					<strong> {t(error.errorKey)}</strong>
+					{error.detailsKey && <p>{t(error.detailsKey)}</p>}
+				</div>
+			);
+		});
 
 	return (
 		<div>
 			<main>
 				<h1>{title}</h1>
 				{children}
+				{errors && getErrorBoxes()}
 			</main>
 		</div>
 	);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,39 @@
 import { useTranslation } from "next-i18next";
-import { useState } from "react";
+import { FormEvent, FormEventHandler, useState } from "react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { UserDialog } from "../components/UserDialog";
 import { GetServerSideProps as PropsFunction, GetServerSidePropsContext as Context } from "next";
 import { LabelInput } from "../components/LabeledInput";
+import axios, { AxiosError } from "axios";
+import Error from "../models/error";
 
 const IndexPage = () => {
 	const [isSignUp, setSignUp] = useState<boolean>(true);
+	const [errors, setErrors] = useState<Error[]>([]);
 
 	const { t } = useTranslation();
+
+	const onSubmitMainForm: FormEventHandler = async (event: FormEvent) => {
+		event.preventDefault();
+
+		const { displayName, password, confirmPassword }: any = event.target as any;
+
+		try {
+			const requestUrl: string = isSignUp ? "/api/users/signup" : "/api/users/login",
+				dataObject = {
+					displayName: displayName.value,
+					password: password.value
+				};
+
+			if (confirmPassword) dataObject["confirmPassword"] = confirmPassword.value;
+
+			await axios.post(requestUrl, dataObject);
+			location.assign("/chat/");
+		} catch (error) {
+			const { response }: AxiosError<Error[]> = error;
+			setErrors(response.data);
+		}
+	};
 
 	return (
 		<UserDialog title={isSignUp ? t("indexpage:pageTitle.signUp") : t("indexpage:pageTitle.logIn")}>
@@ -17,7 +42,7 @@ const IndexPage = () => {
 				<button onClick={() => setSignUp(false)}>{t("indexpage:modeTitles.logIn")}</button>
 			</div>
 
-			<form>
+			<form onSubmit={onSubmitMainForm}>
 				{isSignUp ? (
 					<>
 						<LabelInput type="text" name="displayName" label={t("indexpage:inputs.displayName")} required={true} />
