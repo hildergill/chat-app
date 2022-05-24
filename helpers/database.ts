9e3bb0e4c65c562fe71a6ea3f2326cb0c75b5607
hildergill <hildergill@gmail.com>
2022-05-24 22:03:56 +0800

    Deleted the old database helper file

--- helpers/database.ts
+++
@@ -1,8 +0,0 @@
-import { Connection, createConnection } from "mysql";
-
-export const createDatabaseConnection = (): Connection => {
-	const { DATABASE_HOSTNAME: host, DATABASE_USERNAME: user, DATABASE_PASSWORD: password } = process.env,
-		database: string = "chat_app";
-
-	return createConnection({ host, user, password, database });
-};
