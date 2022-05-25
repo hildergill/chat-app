b9de3e35faf4bec924838cedbc8c0ff752d4b6ac
hildergill <hildergill@gmail.com>
2022-05-25 08:00:10 +0800

    Backup commit

--- backend/singletons/servers.ts
+++ backend/singletons/servers.ts
@@ -1,26 +1,44 @@
-import express, { Express, Request, Response } from "express";
+import express, { Handler } from "express";
+import { Express, Request, Response, json, urlencoded } from "express";
 import { Server as HttpServer, createServer as createHttpServer } from "http";
+import { NextServer } from "next/dist/server/next";
+import CookieParser from "cookie-parser";
+import next from "next";
 
 class Servers {
 	private static instance: Servers;
 
 	private expressServer: Express;
 	private httpServer: HttpServer;
+	private nextServer: NextServer;
 
 	constructor() {
+		const { NODE_ENV } = process.env;
+
 		this.expressServer = express();
 		this.httpServer = createHttpServer(this.expressServer);
+		this.nextServer = next({ dev: NODE_ENV === "development" });
 	}
 
 	public startServer(): void {
-		const { BACKEND_PORT } = process.env;
+		const { BACKEND_PORT, BACKEND_SECRET } = process.env;
 
+		const requestHandler = this.nextServer.getRequestHandler();
 		const port: number = Number(BACKEND_PORT);
 
-		// TODO Add something here later
+		this.nextServer.prepare().then(() => {
+			this.expressServer.use(json(), urlencoded({ extended: false }));
+			this.expressServer.use(CookieParser(BACKEND_SECRET));
 
-		this.httpServer.listen(3000, () => {
-			console.log(`Listening to port ${port}...`);
+			// TODO Add something here later
+
+			this.expressServer.all("*", (request: Request, response: Response) => {
+				return requestHandler(request, response);
+			});
+
+			this.httpServer.listen(3000, () => {
+				console.log(`Listening to port ${port}...`);
+			});
 		});
 	}
 
