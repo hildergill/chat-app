57ba3d2badaff08d08f5d40696a565e30edab4c1
hildergill <hildergill@gmail.com>
2022-05-25 08:28:43 +0800

    Added the basic markup and stuff

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,10 +1,32 @@
-import { useMemo } from "react";
+import { FormEvent, FormEventHandler, useMemo } from "react";
 import { Socket, io } from "socket.io-client";
 
 const ChatPage = () => {
 	const socketClient: Socket = useMemo(() => io(), []);
 
-	return <></>;
+	const onSubmitMessageForm: FormEventHandler = (event: FormEvent) => {
+		event.preventDefault();
+
+		const messageString: string = (event.target as any).message.value;
+
+		console.log(messageString);
+	};
+
+	return (
+		<div>
+			<div>
+				{/* TODO Add something here later */}
+				{/* TODO Add something here later */}
+				{/* TODO Add something here later */}
+			</div>
+
+			<form onSubmit={onSubmitMessageForm}>
+				<label htmlFor="message">Message:</label>
+				<input type="text" name="message" id="message" required />
+				<input type="submit" value="Send Message" />
+			</form>
+		</div>
+	);
 };
 
 export default ChatPage;
