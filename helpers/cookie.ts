c22a1ba679d4cd6979735ae53e3cf0601f819da6
hildergill <hildergill@gmail.com>
2022-05-25 10:25:08 +0800

    Some fixes

--- helpers/cookie.ts
+++ helpers/cookie.ts
@@ -2,9 +2,13 @@ import { CookieOptions } from "express";
 
 export const getUserTokenCookieName = (): string => "USER_TOKEN";
 
-export const getCookieOptions = (): CookieOptions => ({
-	secure: true,
-	signed: true,
-	maxAge: 1209600000,
-	sameSite: true
-});
+export const getCookieOptions = (): CookieOptions => {
+	const { NODE_ENV } = process.env;
+
+	return {
+		secure: NODE_ENV === "production",
+		signed: true,
+		maxAge: 1209600000,
+		sameSite: true
+	};
+};
