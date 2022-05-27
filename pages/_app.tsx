36a0439d75fb8b9de973819623b6370632178d20
hildergill <hildergill@gmail.com>
2022-05-27 16:25:40 +0800

    Added the global style rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 
+import "../stylesheets/global.scss";
+
 const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 export default appWithTranslation(App);
