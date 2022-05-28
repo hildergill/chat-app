bca2845a632220f391f4f2b534256d0e16bea3be
hildergill <hildergill@gmail.com>
2022-05-28 11:12:47 +0800

    Added the stylings for the MessageBox thingy

--- components/messagebox.tsx
+++ components/messagebox.tsx
@@ -1,12 +1,13 @@
 import Message from "../models/message";
 import moment from "moment";
+import MessageBoxStyle from "../stylesheets/components/messagebox.module.scss";
 
 export const MessageBox = (props: Message) => {
 	const { displayName, timestamp, content } = props;
 
 	return (
-		<div>
-			<span>
+		<div className={MessageBoxStyle.messageBox}>
+			<span className={MessageBoxStyle.infoSection}>
 				<strong>{displayName}</strong>
 				<code>{moment(timestamp).format("DD-MM-YYYY hh:mm:ss")}</code>
 			</span>
