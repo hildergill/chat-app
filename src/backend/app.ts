1c16ec35c1edf33071dcf9358c8e456097df2df7
hildergill <hildergill@gmail.com>
2022-06-03 21:02:36 +0800

    Added the function to run the main app function

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,9 +1,17 @@
 import { Connection, createConnection, ConnectionConfig } from "mysql";
+import express, { Express, Request, Response, json, urlencoded } from "express";
+import { Server as HttpServer, createServer as createHttpServer } from "http";
+import createNextServer, { NextServer, NextServerOptions } from "next/dist/server/next";
+import { resolve } from "path";
 
 class App {
 	private static instance: App;
 
 	private databaseConnection: Connection;
+
+	private expressServer: Express;
+	private httpServer: HttpServer;
+	private nextServer: NextServer;
 
 	constructor() {
 		const { DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD } = process.env,
@@ -15,6 +23,38 @@ class App {
 			};
 
 		this.databaseConnection = createConnection(connectionConfig);
+
+		const { NODE_ENV } = process.env,
+			serverConfig: NextServerOptions = {
+				dev: NODE_ENV === "development",
+				dir: resolve("src", "frontend"),
+				quiet: NODE_ENV !== "development"
+			};
+
+		this.expressServer = express();
+		this.httpServer = createHttpServer(this.expressServer);
+		this.nextServer = createNextServer(serverConfig);
+	}
+
+	public runApp(): void {
+		const { BACKEND_PORT } = process.env,
+			port: number = Number(BACKEND_PORT);
+
+		const requestHandler = this.nextServer.getRequestHandler();
+
+		this.nextServer.prepare().then(() => {
+			this.expressServer.use(json(), urlencoded({ extended: false }));
+
+			// TODO Add something here later
+
+			this.expressServer.all("*", (req: Request, res: Response) => {
+				return requestHandler(req, res);
+			});
+
+			this.httpServer.listen(port, () => {
+				console.log(`Listening to port ${port}...`);
+			});
+		});
 	}
 
 	public get DatabaseConnection(): Connection {
