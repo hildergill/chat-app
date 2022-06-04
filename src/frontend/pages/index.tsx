3825c13a11c1ee0f04b107332036c91657cb47a0
hildergill <hildergill@gmail.com>
2022-06-04 23:41:03 +0800

    This is good

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -1,5 +1,19 @@
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { GetStaticProps, GetStaticPropsContext } from "next";
+import { useTranslation } from "next-i18next";
+
 const IndexPage = () => {
-	return <h1>Hello, World!</h1>;
+	const { t } = useTranslation();
+
+	return <h1>{t("common:appName")}</h1>;
+};
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+	return {
+		props: {
+			...(await serverSideTranslations(context.locale))
+		}
+	};
 };
 
 export default IndexPage;
