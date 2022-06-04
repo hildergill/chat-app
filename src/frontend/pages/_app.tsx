95780655ff9be88ec9fc0d1072beb865afc564fa
hildergill <hildergill@gmail.com>
2022-06-04 23:34:02 +0800

    Added the localiization stuff

---
+++ src/frontend/pages/_app.tsx
@@ -0,0 +1,5 @@
+import { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
+
+const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+export default appWithTranslation(App);
