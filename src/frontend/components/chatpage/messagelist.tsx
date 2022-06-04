f01b1a48e382da8bcfb9c6280d8e826f8b313572
hildergill <hildergill@gmail.com>
2022-06-04 11:25:16 +0800

    Some backup commit

--- src/frontend/components/chatpage/messagelist.tsx
+++ src/frontend/components/chatpage/messagelist.tsx
@@ -1,25 +1,34 @@
 import { useContext, useEffect, useState } from "react";
 import { ChatPageContext } from "../../pages/chat";
 import Events from "../../../../events.json";
+import Message from "../../../models/message";
 
-export const MessageList = () => {
+export type MessageListProps = {
+	initialMessages: Message[];
+};
+
+export const MessageList = (props: MessageListProps) => {
+	const { initialMessages }: MessageListProps = props;
 	const { socketClient } = useContext(ChatPageContext);
-	const [messages, setMessages] = useState<JSX.Element[]>([]);
 
-	useEffect(() => {
-		socketClient.on(Events.message, (displayName: string, content: string) => {
-			setMessages((previousMessages) => {
-				const tempComponent: JSX.Element = (
-					<div key={previousMessages.length}>
-						<strong>{displayName}</strong>
-						<p>{content}</p>
-					</div>
-				);
-
-				return [...previousMessages, tempComponent];
-			});
+	const convertToMessageBox = (messages: Message[]): JSX.Element[] => {
+		const tempElements: JSX.Element[] = messages.map((message: Message, key: number) => {
+			return (
+				<div key={key}>
+					<strong>{message.author}</strong>
+					<p>{message.content}</p>
+				</div>
+			);
 		});
-	}, []);
+
+		return tempElements;
+	};
+
+	const [messages, setMessages] = useState<JSX.Element[]>(convertToMessageBox(initialMessages));
+
+	useEffect(()=>{
+		socketClient.on(Events.message, ()=>{})
+	},[]);
 
 	return <div>{messages}</div>;
 };
--- src/frontend/pages/chat.tsx
+++ src/frontend/pages/chat.tsx
@@ -1,30 +1,31 @@
 import { createContext, useMemo, Context } from "react";
 import { Socket, io } from "socket.io-client";
 import { MessageInput } from "../components/chatpage/messageinput";
-import { MessageList } from "../components/chatpage/messagelist";
+import { MessageList, MessageListProps } from "../components/chatpage/messagelist";
 import { GetServerSideProps } from "next";
 import Message from "../../models/message";
 import { fetchLatestMessages } from "../../helpers/messages";
 
 export type ChatPageContextType = {
 	socketClient: Socket;
-	messages: Message[];
 };
 
 export const ChatPageContext: Context<ChatPageContextType> = createContext(null);
 
 type Props = {
-	initialMessage: Message[];
+	initialMessages: Message[];
 };
 
 const ChatPage = (props: Props) => {
-	const { initialMessage: messages }: Props = props;
+	const { initialMessages }: Props = props;
 	const socketClient: Socket = useMemo<Socket>(() => io(), []);
 
+	const messageListProps: MessageListProps = { initialMessages };
+
 	return (
-		<ChatPageContext.Provider value={{ socketClient, messages }}>
+		<ChatPageContext.Provider value={{ socketClient }}>
 			<div>
-				<MessageList />
+				<MessageList {...messageListProps} />
 				<MessageInput />
 			</div>
 		</ChatPageContext.Provider>
@@ -35,9 +36,9 @@ type ServerSideProps = GetServerSideProps<Props>;
 
 export const getServerSideProps: ServerSideProps = async () => {
 	try {
-		const initialMessage: Message[] = await fetchLatestMessages();
+		const initialMessages: Message[] = await fetchLatestMessages();
 
-		return { props: { initialMessage } };
+		return { props: { initialMessages } };
 	} catch (error) {
 		console.error(error);
 		return { notFound: true };
