7485c741550ca6cec7c427b3b430949829b1d9e7
hildergill <hildergill@gmail.com>
2022-06-05 14:36:28 +0800

    This is good

--- src/frontend/pages/index.tsx
+++ src/frontend/pages/index.tsx
@@ -1,11 +1,23 @@
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { GetStaticProps, GetStaticPropsContext } from "next";
 import { useTranslation } from "next-i18next";
+import { UserDialog } from "../components/userdialog";
+import { useState } from "react";
 
 const IndexPage = () => {
+	const [isSignUp, setSignUp] = useState<boolean>(true);
+
 	const { t } = useTranslation();
 
-	return <h1>{t("common:appName")}</h1>;
+	const pageTitle: string = t(isSignUp ? "indexpage:pageTitles.signUp" : "indexpage:pageTitles.logIn");
+
+	return (
+		<UserDialog title={pageTitle}>
+			{/* TODO Add something here later */}
+			{/* TODO Add something here later */}
+			{/* TODO Add something here later */}
+		</UserDialog>
+	);
 };
 
 export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
