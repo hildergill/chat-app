695d26ac7bb344a710cc1ba4dc302a24507f7ea8
hildergill <hildergill@gmail.com>
2022-06-08 07:13:00 +0800

    Moved the frontend stuff to its own directory

--- frontend/components/messagebox.tsx
+++ frontend/components/messagebox.tsx
@@ -1,7 +1,7 @@
 // This file is a part of chat-app (https://www.github.com/hildergill/chat-app)
 // Copyright 2022 Hilder Gill
 
-import Message from "../models/message";
+import Message from "../../models/message";
 import moment from "moment";
 import MessageBoxStyle from "../stylesheets/components/messagebox.module.scss";
 
