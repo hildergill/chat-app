e39206e98ae52ebca81a4778910583359668c928
hildergill <hildergill@gmail.com>
2022-06-24 19:03:34 +0800

    Added the devcustom file

---
+++ scripts/devcustom.js
@@ -0,0 +1,7 @@
+// This file is a part of chat-app (https://www.github.com/hildergill/chat-app)
+// Copyright 2022 Hilder Gill
+
+const nodemon = require("nodemon");
+const devconfig = require("./devconfig");
+
+nodemon(devconfig);
