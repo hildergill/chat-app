d578f51c8c2e8d9036d1f4b19623033ea01ccde6
hildergill <hildergill@gmail.com>
2022-07-07 19:49:57 +0800

    Changed some of the database attributes

--- database/init.sql
+++ database/init.sql
@@ -4,7 +4,7 @@
 create database `chat_app`;
 
 create table `chat_app`.`users` (
-    `id` char(36) not null,
+    `id` binary(16) not null default (uuid_to_bin(uuid())),
     `display_name` varchar(256) not null,
     `password` text not null,
 
@@ -14,17 +14,17 @@ create table `chat_app`.`users` (
 
 create table `chat_app`.`user_tokens` (
     `token` varchar(512) not null,
-    `user` char(36) not null,
+    `user` binary(16) not null,
 
     primary key (`token`),
     foreign key (`user`) references `chat_app`.`users` (`id`)
 );
 
 create table `chat_app`.`messages` (
-    `id` char(36) not null,
-    `author` char(36) not null,
+    `id` binary(16) not null default (uuid_to_bin(uuid())),
+    `author` binary(16) not null,
     `content` text not null,
-    `timestamp` bigint not null,
+    `timestamp` datetime not null default (utc_timestamp()),
 
     primary key (`id`),
     foreign key (`author`) references `chat_app`.`users` (`id`)
