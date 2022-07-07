e3c78ad97c4956fdcf70e2318ace097da616c2cb
hildergill <hildergill@gmail.com>
2022-07-07 21:11:38 +0800

    Overhauled convertValidationError()

--- src/helpers/errors.ts
+++ src/helpers/errors.ts
@@ -2,10 +2,7 @@
 // Copyright 2022 Hilder Gill
 
 import Joi from "joi";
-import Error from "../models/error";
 
-export const convertValidationError = (errors: Joi.ValidationError): Error[] => {
-	return errors.details.map((item: Joi.ValidationErrorItem) => {
-		return { errorKey: item.message };
-	});
+export const convertValidationError = (errors: Joi.ValidationErrorItem[]): string[] => {
+	return errors.map((error: Joi.ValidationErrorItem) => error.message);
 };
--- src/models/error.ts
+++
@@ -1,9 +0,0 @@
-// This file is a part of chat-app (https://www.github.com/hildergill/chat-app)
-// Copyright 2022 Hilder Gill
-
-type Error = {
-	errorKey: string;
-	detailsKey?: string;
-};
-
-export default Error;
