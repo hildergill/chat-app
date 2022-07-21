0f766f1f4bf7f7f996f59456407e6980949a2ae1
hildergill <hildergill@gmail.com>
2022-07-21 12:03:06 +0800

    Readded server.ts

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -1,8 +1,41 @@
 // This file is a part of chat-app (https://www.github.com/hildergill/chat-app)
 // Copyright 2022 Hilder Gill
 
+import express, { Express, Request, Response } from "express";
+import { json, urlencoded } from "express";
 import { config } from "dotenv";
-import ServersSingleton from "./app";
+import { resolve } from "path";
+import cookieParser from "cookie-parser";
+import next from "next";
+import ApiRoutes from "../../apiroutes.json";
+import UsersRoute from "./routes/usersroute";
+import MessagesRoute from "./routes/messagesroute";
 
 config();
-ServersSingleton.startServer();
+
+const { NODE_ENV, BACKEND_PORT, BACKEND_SECRET } = process.env,
+	port: number = parseInt(BACKEND_PORT!),
+	dev: boolean = NODE_ENV === "development";
+
+const expressServer: Express = express(),
+	nextServer = next({ dev, dir: resolve("src", "frontend") });
+
+const requestHandler = nextServer.getRequestHandler();
+
+nextServer.prepare().then(() => {
+	expressServer.use(json(), urlencoded({ extended: false }));
+	expressServer.use(cookieParser(BACKEND_SECRET!));
+
+	expressServer.use(ApiRoutes.users, UsersRoute);
+	expressServer.use(ApiRoutes.messages, MessagesRoute);
+
+	// TODO Add something here later
+
+	expressServer.all("*", (req: Request, res: Response) => {
+		return requestHandler(req, res);
+	});
+
+	expressServer.listen(port, () => {
+		console.log(`Listening to port ${port}`);
+	});
+});
