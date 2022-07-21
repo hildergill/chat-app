f70b9c2f69c3e9ac8d7bc5972233b3b6b066a30f
hildergill <hildergill@gmail.com>
2022-07-21 18:51:41 +0800

    Removed references to App.ts in the helpers directory

--- src/helpers/users.ts
+++ src/helpers/users.ts
@@ -3,25 +3,25 @@
 
 import { hash } from "bcrypt";
 import User from "../models/users/user";
-import App from "../backend/app";
+import { Connection } from "mysql";
 
-export const createUser = (displayName: string, unhashedPassword: string): Promise<void> =>
+export const createUser = (database: Connection, displayName: string, unhashedPassword: string): Promise<void> =>
 	new Promise<void>(async (resolve, reject) => {
 		const hashedPassword: string = await hash(unhashedPassword, 12),
 			queryParams: string[] = [displayName, hashedPassword],
 			queryString: string = "insert into users values (?, ?)";
 
-		App.DatabaseConnection.query(queryString, queryParams, (error) => {
+		database.query(queryString, queryParams, (error) => {
 			if (error) return reject(error);
 			return resolve();
 		});
 	});
 
-export const fetchUsers = (): Promise<User[]> =>
+export const fetchUsers = (database: Connection): Promise<User[]> =>
 	new Promise<User[]>((resolve, reject) => {
 		const queryString: string = "select * from users order by display_name asc";
 
-		App.DatabaseConnection.query(queryString, (error: any, results: any[]) => {
+		database.query(queryString, (error: any, results: any[]) => {
 			if (error) return reject(error);
 
 			return resolve(
@@ -33,11 +33,11 @@ export const fetchUsers = (): Promise<User[]> =>
 		});
 	});
 
-export const fetchUserByDisplayName = (displayName: String): Promise<User> =>
+export const fetchUserByDisplayName = (database: Connection, displayName: String): Promise<User> =>
 	new Promise<User>((resolve, reject) => {
 		const queryString: string = "select * from users where display_name = ?";
 
-		App.DatabaseConnection.query(queryString, displayName, (error, results: any[]) => {
+		database.query(queryString, displayName, (error, results: any[]) => {
 			if (error) return reject(error);
 			if (results.length !== 1) return reject(0);
 
